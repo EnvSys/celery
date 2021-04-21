2111392fe57c1099ca03106c2a2c5dacb7a8682c
Maximilian Friedersdorff <max@friedersdorff.com>
2021-04-21 14:17:33 +0100

    Remove forgotten pprint import

--- celery/backends/base.py
+++ celery/backends/base.py
@@ -37,8 +37,6 @@ from celery.utils.serialization import (create_exception_cls,
                                         raise_with_context)
 from celery.utils.time import get_exponential_backoff_interval
 
-import pprint
-
 __all__ = ('BaseBackend', 'KeyValueStoreBackend', 'DisabledBackend')
 
 EXCEPTION_ABLE_CODECS = frozenset({'pickle'})
